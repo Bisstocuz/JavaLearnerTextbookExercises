826c04dcaeecc1e772502bafc75c24fb56d9cab7
Notealot <42398278+Notealot@users.noreply.github.com>
2020-11-30 10:37:11 +0800

    little fix

--- Chapter10/exercise12.java
+++ Chapter10/exercise12.java
@@ -1,15 +1,12 @@
 package Chapter10;
 
 class Teacher implements Runnable {
-    private static final Object lock = new Object();
     private int notes = 80;
 
     @Override
     public void run() {
-        synchronized (lock) {
-            while (notes > 0) {
-                System.out.println(Thread.currentThread().getName() + "正在分发第" + notes-- + "份学习笔记。");
-            }
+        while (notes > 0) {
+            System.out.println(Thread.currentThread().getName() + "正在分发第" + notes-- + "份学习笔记。");
         }
     }
 }
